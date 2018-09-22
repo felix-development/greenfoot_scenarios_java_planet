de0ef340f240ba2d9f6c47e4d862c7edd87ae0fb
felix-development <felixprivateskonto@gmail.com>
2018-09-22 13:25:00 +0200

    planet: fix saving the world

--- Planet.java
+++ Planet.java
@@ -15,11 +15,27 @@ public class Planet extends World
     public Planet()
     {
         super(20, 20, zellenGroesse);
-         setBackground("images/boden.png");
+        setBackground("images/boden.png");
         setPaintOrder(String.class, Rover.class, Marke.class, Gestein.class, Huegel.class);
         Greenfoot.setSpeed(20); 
+        prepare();
     }
 
-  
-    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Huegel huegel = new Huegel();
+        addObject(huegel,9,5);
+        Huegel huegel2 = new Huegel();
+        addObject(huegel2,10,8);
+        Huegel huegel3 = new Huegel();
+        addObject(huegel3,7,8);
+        Huegel huegel4 = new Huegel();
+        addObject(huegel4,6,11);
+        Huegel huegel5 = new Huegel();
+        addObject(huegel5,8,12);
+    }
 }
